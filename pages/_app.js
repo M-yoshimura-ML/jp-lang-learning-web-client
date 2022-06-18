2ab3de616fc1fe8d22527e846947e643781e5d75
M-yoshimura-ML <masahiro.yoshimura@freemight.com>
2022-06-18 18:42:10 +0800

    test GA tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import { AuthProvider } from '../context/AuthContext'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9LLYFBFMNR"></Script>
+      <Script id="google-analytics">
+        {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+        
+                gtag('config', 'G-9LLYFBFMNR');
+        `}
+      </Script>
       <Component {...pageProps} /> 
     </AuthProvider>
   )
